16eb29c1a0fdd702e421ceb841e68adc47ec8c17
bibi-git <84048715@qq.com>
2017-07-28 01:24:09 +0800

    增加模块

---
+++ boot03/src/main/java/org/bibi/boot03/Boot03Application.java
@@ -0,0 +1,16 @@
+package org.bibi.boot03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author：Tom
+ * Date：2017/7/27
+ * Email：84048715@qq.com
+ */
+@SpringBootApplication
+public class Boot03Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Boot03Application.class, args);
+    }
+}
---
+++ boot04/src/main/java/org/bibi/boot04/Boot04Application.java
@@ -0,0 +1,16 @@
+package org.bibi.boot04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author：Tom
+ * Date：2017/7/27
+ * Email：84048715@qq.com
+ */
+@SpringBootApplication
+public class Boot04Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Boot04Application.class, args);
+    }
+}
